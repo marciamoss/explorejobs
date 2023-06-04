b1b9509018aecce072e44804e017b1e35e6a81d4
Marcia Moss <marcia_moss@hotmail.com>
2023-06-03 20:30:11 -0400

    persist reducer added

--- store/index.js
+++ store/index.js
@@ -1,5 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+import { combineReducers } from "redux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { authReducer, authInfo } from "./slices/authSlice";
 import { jobsReducer, jobsInfo, jobsListing } from "./slices/jobsSlice";
@@ -9,15 +19,33 @@ import {
   clearLikedJobs,
 } from "./slices/likedJobsSlice";
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    jobs: jobsReducer,
-    likedJobs: likedJobsReducer,
-  },
+const rootReducer = combineReducers({
+  auth: authReducer,
+  jobs: jobsReducer,
+  likedJobs: likedJobsReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: ["likedJobs"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
 export { authInfo, jobsInfo, liked, jobsListing, clearLikedJobs };
 export * from "./thunks/authApis";
 export * from "./thunks/jobApis";
 export * from "./thunks/likedJobsApis";
+persistStore(store);
 export default store;
